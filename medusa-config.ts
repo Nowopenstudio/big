8dd12227ff97682b914a9a1079d736791715e30e
Ro Oeurn <ro@nowopen.studio>
2026-04-22 15:18:08 -0700

    new

--- medusa-config.ts
+++ medusa-config.ts
@@ -3,17 +3,10 @@ import { loadEnv, defineConfig } from '@medusajs/framework/utils'
 loadEnv(process.env.NODE_ENV || 'development', process.cwd())
 
 module.exports = defineConfig({
-  admin: {
-    // ...
-    backendUrl: process.env.MEDUSA_BACKEND_URL,
-  },
   projectConfig: {
-    redisUrl: process.env.REDIS_URL,
-    workerMode: process.env.MEDUSA_WORKER_MODE as "shared" | "worker" | "server",
     databaseUrl: process.env.DATABASE_URL,
-    databaseDriverOptions: process.env.NODE_ENV !== "development" ?
-       { ssl: { rejectUnauthorized: false }, pool: { min: 2, max: 30 },idle_in_transaction_session_timeout:30000 }:{},
-    
+  
+
     http: {
       storeCors: process.env.STORE_CORS!,
       adminCors: process.env.ADMIN_CORS!,
@@ -22,7 +15,6 @@ module.exports = defineConfig({
       cookieSecret: process.env.COOKIE_SECRET || "supersecret",
     }
   },
-  
   modules: [
     {
       resolve: "./src/modules/sanity",
@@ -38,52 +30,5 @@ module.exports = defineConfig({
         },
       },
     },
-    {
-      resolve: "@medusajs/medusa/caching",
-      options: {
-        providers: [
-          {
-            resolve: "@medusajs/caching-redis",
-            id: "caching-redis",
-            is_default: true,
-            options: {
-              redisUrl: process.env.CACHE_REDIS_URL,
-            },
-          },
-        ],
-      },
-    },
-    {
-      resolve: "@medusajs/medusa/event-bus-redis",
-      options: {
-        redisUrl: process.env.REDIS_URL,
-      },
-    },
-    {
-      resolve: "@medusajs/medusa/workflow-engine-redis",
-      options: {
-        redis: {
-          // Note: This was `url` before v2.12.2
-          // It's now deprecated in favor of `redisUrl`
-          redisUrl: process.env.REDIS_URL,
-        },
-      },
-    },
-    {
-      resolve: "@medusajs/medusa/locking",
-      options: {
-        providers: [
-          {
-            resolve: "@medusajs/medusa/locking-redis",
-            id: "locking-redis",
-            is_default: true,
-            options: {
-              redisUrl: process.env.LOCKING_REDIS_URL,
-            },
-          },
-        ],
-      },
-    },
-
   ],
 })
